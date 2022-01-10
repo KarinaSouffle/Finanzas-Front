6746c17dcb4c9f3fe74cb016f77bb6fe7d5c3796
KarinaSouffle <karinasouffle@gmail.com>
2022-01-10 08:24:43 -0700

    Update

--- src/components/AreasDeerland.js
+++ src/components/AreasDeerland.js
@@ -23,6 +23,12 @@ export default function Nomina() {
   const columns = [
     { field: "ID_A", headerName: "ID", width: 50 },
     {
+      field: 'ID_A',
+      headerName: 'ID Area',
+      width: 150,
+      editable: true,
+    },
+    {
       field: 'Nombre_A',
       headerName: 'Nombre Area',
       width: 150,
