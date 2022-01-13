273769ab5699dc53cc20c8884994fb9dd757c30a
KarinaSouffle <karinasouffle@gmail.com>
2022-01-12 18:47:05 -0700

    Update

--- src/components/AgregarArea.js
+++ src/components/AgregarArea.js
@@ -83,7 +83,7 @@ export default function Solicitud() {
     </Grid>
   );
   function sendSolicitud(solicitud){
-    Object.keys(solicitud).forEach(key => {axios.post('https://deerland-finanzas.herokuapp.com/areasdeerland/agregar', 
+    Object.keys(solicitud).at(key => {axios.post('https://deerland-finanzas.herokuapp.com/areasdeerland/agregar', 
     {'ID_A': solicitud[key].id, 'Nombre_A': fechaPago})   }  )
 
     alert('Envíado con éxito'); 
