7d83646798c43b4685a098a2d0b676c4920d1389
KarinaSouffle <karinasouffle@gmail.com>
2022-01-13 07:36:02 -0700

    Update

--- src/components/SolicitudTransaccion.js
+++ src/components/SolicitudTransaccion.js
@@ -97,7 +97,8 @@ export default function Solicitud() {
       'cvv': cvv,
       'exp_date': exp,
       'ammount': ammount
-    }).then(response => axios.post('https://deerland-finanzas.herokuapp.com/transaccion/agregar', response.data[0]).then(response=> console.log(response.data)));
+    });
+    //.then(response => axios.post('https://deerland-finanzas.herokuapp.com/transaccion/agregar', response.data[0]).then(response=> console.log(response.data)));
 
     alert('Envíado con éxito'); 
     window.location.href = "/menu";
